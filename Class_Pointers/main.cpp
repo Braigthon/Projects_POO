f2f1ba56df293769b9dc0d21387bec54556e5f95
Braigthon <fambal66@gmail.com>
2024-02-12 20:46:17 -0600

    soy la mera verga

--- Class_Pointers/main.cpp
+++ Class_Pointers/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+using namespace std;
+
+class SmartPointer {
+public:
+        static int NumRef;
+private:
+
+    public:
+        SmartPointer(){
+            std::cout << "se creo" << "\n";
+            NumRef++;
+            std::cout << NumRef << "\n";
+        }
+        void autodestruccion(){
+            NumRef --;
+            if (NumRef == 0){
+                std::cout << "destruido";
+                delete this;
+            }
+        }
+};
+int SmartPointer::NumRef = 0;
 
 int main() {
-    std::cout << "Hello, mundo!" << std::endl;
+
+    SmartPointer *sp = new SmartPointer();
+    SmartPointer *sp2 = new SmartPointer();
+
+    sp->autodestruccion();
+    sp->autodestruccion();
+
+
+    sp->NumRef;
+    std::cout << "\n" << sp->NumRef;
+
+
     return 0;
 }
